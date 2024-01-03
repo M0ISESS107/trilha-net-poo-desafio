ae338b2bfcb83ecc7d00f2a565d63c498f75ed74
moises mauricio <moisesaurora107@gmail.com>
2024-01-03 03:36:48 -0300

    exercicio finalizado

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,25 @@
+using DesafioPOO.Models;
+
+
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia (string numero, string modelo,string mei,int memoria): base(numero,modelo,mei,memoria)
+        // TODO: Sobrescrever o método "InstalarAplicativo
+        {
+
+        }
+
+        public  override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
+
+
+        }
+
+       
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine($"meu telefone Nokia");
+Smartphone nokia = new Nokia(numero:"81 998223-1432",modelo:"modelo-1",mei:"1111",memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo($"Voice");
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"meu Iphone");
+Smartphone IPhone13 = new Iphone(numero:"81 99223-4532", modelo:"modelo-13", mei:"4444", memoria:256);
+nokia.Ligar();
+IPhone13.InstalarAplicativo($"Music Download");
